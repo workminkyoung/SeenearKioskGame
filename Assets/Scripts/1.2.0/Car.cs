5e87526e5b7733ab1a064077f21b606e7970eed2
mkkimDesktop <workminkyoung@gmail.com>
2025-04-11 00:46:07 +0900

    feat: car movement

--- Assets/Scripts/1.2.0/Car.cs
+++ Assets/Scripts/1.2.0/Car.cs
@@ -1,17 +1,89 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Car : MonoBehaviour
+public class Car : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField]
     private List<Sprite> _sprites;
     [SerializeField]
     private Image _image;
+    [SerializeField]
+    private RectTransform _carRect;
+    [SerializeField]
+    private GameObject _guide;
+    [SerializeField]
+    private RectTransform _canvas;
+    [SerializeField]
+    private float _moveDuration;
+
+    private float _posY;
+    private bool _isRefueling = false;
 
     public void Init()
     {
-        _image = GetComponent<Image>();
+        _carRect = GetComponent<RectTransform>();
         _image.sprite = _sprites[0];
+        _posY = _carRect.anchoredPosition.y;
+    }
+
+    public void CarSpawn(int carType)
+    {
+        _image.sprite = _sprites[carType];
+        _image.SetNativeSize();
+        _carRect.anchoredPosition = new Vector2(540 + _image.rectTransform.sizeDelta.x/2, _posY);
+        _guide.SetActive(false); 
+        _isRefueling = false;
+
+    }
+
+    public void StartMoveIn()
+    {
+        StartCoroutine(MoveTo(_carRect, new Vector2(0, _posY), _moveDuration, () =>
+        {
+            _guide.SetActive(true); 
+            _isRefueling = true;
+        }));
+    }
+
+    public void StartMoveOut()
+    {
+        StartCoroutine(MoveTo(_carRect, new Vector2(-540 - _image.rectTransform.sizeDelta.x / 2, _posY), _moveDuration, () =>
+        {
+            _isRefueling = false;
+        }));
+    }
+
+    private IEnumerator MoveTo(RectTransform rect, Vector2 targetPos, float duration, Action action = null)
+    {
+        Vector2 startPos = rect.anchoredPosition;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            rect.anchoredPosition = Vector2.Lerp(startPos, targetPos, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        rect.anchoredPosition = targetPos;
+        action?.Invoke();
+    }
+
+    // UI 요소를 누를 때 호출됨
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!_isRefueling) return;
+        _guide.SetActive(false);
+    }
+
+    // UI 요소에서 손을 뗄 때 호출됨
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!_isRefueling) return;
+        _guide.SetActive(true);
     }
 }
--- Assets/Scripts/1.2.0/GasGame.cs
+++ Assets/Scripts/1.2.0/GasGame.cs
@@ -6,41 +6,10 @@ using DG.Tweening;
 public class GasGame : MonoBehaviour
 {
     [SerializeField]
-    private List<Image> _decors = new List<Image>();
-    [SerializeField]
     private SwitchImage _gas;
 
     private void Awake()
     {
         
-    }
-
-    private void IntroAnimation()
-    {
-
-    }
-
-    private void LoopingAnimation()
-    {
-
-    }
-
-    private void StopAnimation()
-    {
-
-    }
-
-    private enum Decor
-    {
-        None = -1,
-
-        Stars,
-        Moon,
-        Cloud1,
-        Cloud2,
-        Window,
-        Building,
-
-        Length
     }
 }
