6971fcdba2157835151a871d03db26edb0112851
mkkimDesktop <workminkyoung@gmail.com>
2025-06-18 00:11:28 +0900

    feat: 작물 타입별 활성화 세팅

--- Assets/Scripts/1.2.0/Crops.cs
+++ Assets/Scripts/1.2.0/Crops.cs
@@ -9,8 +9,12 @@ public class Crops : MonoBehaviour
     [SerializeField] private Image _soilFront;
     [SerializeField] private Image _soilBack;
     
+    private float _soilFrontSizeSmall = 220;
+    private float _soilBackSizeSmall = 150;
+    private float _soilFrontSizeBig = 290;
+    private float _soilBackSizeBig = 220;
 
-    private void Init()
+    public void Init()
     {
         for (int i = 0; i < _crops.Count; i++)
         {
@@ -25,10 +29,14 @@ public class Crops : MonoBehaviour
 
         if(cropType <= CropType.Potato){
             // 작은 흙으로 세팅
+            _soilFront.rectTransform.sizeDelta = new Vector2(_soilFrontSizeSmall, _soilFront.rectTransform.sizeDelta.y);
+            _soilBack.rectTransform.sizeDelta = new Vector2(_soilBackSizeSmall, _soilBack.rectTransform.sizeDelta.y);
         }
         else{
             _soilFront.gameObject.SetActive(true);
             // 큰 흙으로 세팅
+            _soilFront.rectTransform.sizeDelta = new Vector2(_soilFrontSizeBig, _soilFront.rectTransform.sizeDelta.y);
+            _soilBack.rectTransform.sizeDelta = new Vector2(_soilBackSizeBig, _soilBack.rectTransform.sizeDelta.y);
         }
     }
 
@@ -39,17 +47,18 @@ public class Crops : MonoBehaviour
         }
     }
 
+}
+
     public enum CropType
     {
         None = -1,
-        Beet,
         Broccoli,
-        Radish,
         Carrot,
-        Onion,
         SweetPotato,
         Potato,
+        Onion,
+        Beet,
+        Radish,
         Pumpkin,
         Cabbage
     }
-}
--- Assets/Scripts/1.2.0/LoadingGame.cs
+++ Assets/Scripts/1.2.0/LoadingGame.cs
@@ -1,40 +1,45 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections.Generic;
 
 public class LoadingGame : MonoBehaviour
 {
     // 인트로 세팅값
-    [SerializeField] private GameObject _car;
+    [SerializeField] private RectTransform _car;
     [SerializeField] private Vector3 _carStartPos;
     [SerializeField] private Vector3 _carPausePos;
     [SerializeField] private Vector3 _carEndPos;
     [SerializeField] private float _carMoveTime;
 
+    [Header("Crop Setting")]
+    [SerializeField] private List<Crops> _crops = new List<Crops>();
+    [SerializeField] private CropType _cropType; //임시
+
     void Awake()
     {
-        
+        _crops.AddRange(GetComponentsInChildren<Crops>());
+        for(int i = 0; i < _crops.Count; i++){
+            _crops[i].Init();
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Intro();
     }
 
     private void Intro(){
-        _car.transform.position = _carStartPos;
-        _car.transform.DOMove(_carPausePos, _carMoveTime).SetEase(Ease.InOutSine).OnComplete(() =>
+        _car.anchoredPosition = _carStartPos;
+        _car.DOAnchorPos(_carPausePos, _carMoveTime).SetEase(Ease.InOutSine).OnComplete(() =>
         {
             StartGame();
         });
     }
 
     private void StartGame(){
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for(int i = 0; i < _crops.Count; i++){
+            _crops[i].SpawnCrop(_cropType);
+        }
     }
 }
